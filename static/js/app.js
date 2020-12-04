89205977beadf33e822bf85cb910ba3ed6af5cbb
improvbutterfly <68628212+improvbutterfly@users.noreply.github.com>
2020-12-04 11:56:24 -0800

    Working on displaying metadata

--- static/js/app.js
+++ static/js/app.js
@@ -3,10 +3,12 @@ const dataPromise = d3.json("samples.json");
 
 // Set up the drop-down menu
 function init() {
+
 	dataPromise.then(function(data) {
 		  console.log(data);
 
 	    var names = data.names;
+	    console.log(names);
 
 	    d3.select("select").selectAll("option")
 	    .data(names)
@@ -19,7 +21,36 @@ function init() {
 	    	return d;
 	    });
 
+
+	    // set default ID to first ID in data
+	   	var defaultID = names[0];
+		console.log(defaultID);
+
+		// update the metadata
+		loadMetadata(defaultID);
+
+		// Plot the data for the first ID
+		buildPlot(defaultID);
+
 	});
+
+};
+
+// function to build the graph
+function loadMetadata(ID){
+	console.log(ID);
+	dataPromise.then(function(data) {
+		  console.log(data);
+
+	    var metadata = data.metadata.filter(md => md.id === parseInt(ID));
+
+	    console.log(metadata);
+
+	    // Empty contents of metadata area
+	    d3.select("#sample-metadata")
+	    .html(`ID: ${metadata[0].id}<br />`);
+
+  	});
 };
 
 // function to unpack data
@@ -30,14 +61,15 @@ function unpack(rows, index) {
 };
 
 // function to build the graph
-function buildPlot(){
+function buildPlot(ID){
+	console.log(ID);
 	dataPromise.then(function(data) {
 		  console.log(data);
 
-	    var names = data.names;
-	    var metadata = data.metadata;
-	    var ids = data.samples.id;
-	    console.log(names);
+	    var metadata = data.metadata.filter(md => md.id === parseInt(ID));
+
+	    console.log(metadata);
+	    //var ids = data.samples.id;
 	    //var endDate = data.dataset.end_date;
 	    //var dates = unpack(data.dataset.data, 0);
 	    //var closingPrices = unpack(data.dataset.data, 4);
